e45e0167570dcdac588c6b799b97b1d3a32038ea
mmahub2006 <yurideazevedosilvagama@gmail.com>
2026-06-12 22:17:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarCuriosidade(){
+
+document.getElementById("curiosidade").innerHTML =
+"O MMA moderno surgiu da combinação de diversas artes marciais como Jiu-Jitsu, Boxe e Wrestling.";
+
+}
